c2fa566e67d9122a1d9a3fea5cb39b1c37dd8828
Sam Burney <sburney@sifnt.net.au>
2020-05-21 13:15:21 +0930

    Initial commit

---
+++ auto-syncoid.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Confirm this script is not currently running
+for pid in $(pidof -x auto-syncoid.sh); do
+    if [ $pid != $$ ]; then
+        echo "Process already running"
+        exit
+    fi
+done
+
+# Config
+declare -A SYNC_SRC_HOST
+declare -A SYNC_SRC_PATH_PREFIX
+declare -A SYNC_SRC_PATHS
+source auto-syncoid.conf
+
+# Do the loop de loop!
+for HOST in ${!SYNC_SRC_HOST[@]} ; do
+    echo "Processing ${SYNC_SRC_HOST[$HOST]}..."
+    for SRC_PATH in ${SYNC_SRC_PATHS[$HOST]} ; do
+        echo "Syncing ${SYNC_SRC_PATH_PREFIX[$HOST]}${SRC_PATH} ==> ${SYNC_DST_PATH}/${SYNC_SRC_HOST[$HOST]}/${SRC_PATH}..."
+        zfs create -p "${SYNC_DST_PATH}/${SYNC_SRC_HOST[$HOST]}/$(dirname ${SRC_PATH})"
+        syncoid -r "${SYNC_SRC_PATH_PREFIX[$HOST]}${SRC_PATH}" "${SYNC_DST_PATH}/${SYNC_SRC_HOST[$HOST]}/${SRC_PATH}"
+    done
+done
